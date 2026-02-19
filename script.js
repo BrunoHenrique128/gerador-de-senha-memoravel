4a51901730920792be8623aec03fac3c9200e64a
BrunoHenrique128 <bhpindaniel@gmail.com>
2026-02-19 14:50:19 -0300

    WIP: adiciona a lógica inicial do JS

---
+++ script.js
@@ -0,0 +1,16 @@
+const substantivos = [];
+const adjetivos = [];
+const numeros1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numeros2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numeros3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const especial = ["*", "@"];
+
+const copyBtn = document.getElementById("copyBtn");
+const button = document.getElementById("generate");
+const password = document.getElementById("password");
+
+function result() {
+
+}
+
+button.addEventListener("click", result);
